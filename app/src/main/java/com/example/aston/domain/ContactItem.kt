e17c788dfcd7bfffa170e4c31606b98092c1635a
pavel <t28039ab@mail.ru>
2023-11-21 14:50:28 +0700

    124

--- app/src/main/java/com/example/aston/domain/ContactItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.aston.domain
-
-data class ContactItem(
-    val contact: Contact,
-    val visible: Boolean,
-    val checked: Boolean
-)
--- app/src/main/java/com/example/aston/presentation/ContactItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aston.presentation
-
-import com.example.aston.domain.Contact
-
-data class ContactItem (
-    val contact: Contact,
-    val selected: Boolean,
-    val visibleCheckBox: Boolean
-)
